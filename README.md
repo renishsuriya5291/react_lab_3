c9e67bd82bd9e32ffb2ba7415a274dbb9560c7c9
renishsuriya5291 <renishsuriya1441@gmail.com>
2024-07-10 11:01:01 +0530

    display Time And date in home page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react_lab_3
+
+This Repository Contain Home, About, Contact and 404 Pages
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 
-function Home(){
+function Home() {
+    const [date, setDate] = useState(new Date().toLocaleDateString())
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+
+    setInterval(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, 1000);
+
     return (
         <>
-        <div className="container">
-            <h1>Home Works</h1>
-        </div>
+            <div className="container">
+                <h1>Home Works</h1>
+                <h2>It is {date}</h2>
+                <h2>It is {time}</h2>
+            </div>
         </>
     );
 }
